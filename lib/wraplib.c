c7d9677eda8cb9c688b4673a6279f3a0213d881e
Applenice <zxapplenice@gmail.com>
2019-02-10 21:58:03 +0800

    add wraplib.c

---
+++ lib/wraplib.c
@@ -0,0 +1,27 @@
+/*
+ * Wrapper functions for our own library functions.
+ * Most are included in the source file for the function itself.
+ */
+
+#include "unp.h"
+
+const char *Inet_ntop(int family, const void *addrptr, char *strptr, size_t len)
+{
+    const char *ptr;
+
+    if (strptr == NULL)                   /* check for old code */
+        printf("NULL 3rd argument to inet_ntop");
+    if ((ptr = inet_ntop(family, addrptr, strptr, len)) == NULL)
+        printf("inet_ntop error");        /* sets errno */
+    return ptr;
+}
+
+void Inet_pton(int family, const char *strptr, void *addrptr)
+{
+    int n;
+
+    if ((n = inet_pton(family, strptr, addrptr)) < 0)
+        printf("inet_pton error for %s", strptr);    /* errno set */
+    else if (n == 0)
+        printf("inet_pton error for %s", strptr);    /* errno not set */
+}

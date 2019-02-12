8e64a108e9fd764ed0aeeafa563097e5d7da6d05
Applenice <zxapplenice@gmail.com>
2019-02-12 22:26:46 +0800

    add sockfd_to_family.c

---
+++ lib/sockfd_to_family.c
@@ -0,0 +1,20 @@
+#include "unp.h"
+
+int sockfd_to_family(int sockfd)
+{
+    struct sockaddr_storage ss;
+    socklen_t len;
+
+    len = sizeof(ss);
+    if (getsockname(sockfd, (struct sockaddr *) &ss, &len) < 0)
+        return -1;
+    return ss.ss_family;
+}
+
+int Sockfd_to_family(int sockfd)
+{
+    int rc;
+    if ((rc = sockfd_to_family(sockfd)) < 0)
+        printf("sockfd_to_family error");
+    return rc;
+}

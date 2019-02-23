ce888e0180cf576435e8fb027368dbf98bde92b6
Applenice <zxapplenice@gmail.com>
2019-02-23 20:40:40 +0800

    add tcpserv01.c

---
+++ tcpcliserv/unp.h
@@ -0,0 +1,21 @@
+#ifndef __unp_h
+#define __unp_h
+
+#define    MAXLINE 4096    /* max text line length */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>    /* basic socket definitions */
+#include <sys/time.h>      /* timeval{} for select() */
+#include <time.h>
+
+#include <netinet/in.h>    /* sockaddr_in{} and other Internet defns */
+#include <arpa/inet.h>     /* inet(3) functions */
+#include <unistd.h>
+
+#define LISTENQ 1024    /* 2nd argument to listen() */
+#define	SERV_PORT 9877  /* TCP and UDP */
+
+#endif

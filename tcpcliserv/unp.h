1cf6cf4e8088848f923ee64c7897767bcbc27ced
Applenice <zxapplenice@gmail.com>
2019-02-23 22:20:21 +0800

    update

--- tcpcliserv/unp.h
+++ tcpcliserv/unp.h
@@ -8,6 +8,7 @@
 #include <sys/socket.h>    /* basic socket definitions */
 #include <sys/time.h>      /* timeval{} for select() */
 #include <time.h>
+#include <errno.h>
 
 #include <netinet/in.h>    /* sockaddr_in{} and other Internet defns */
 #include <arpa/inet.h>     /* inet(3) functions */

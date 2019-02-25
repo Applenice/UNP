43783f285c20396e97f38d1a1a66529cd084e055
Applenice <zxapplenice@gmail.com>
2019-02-25 23:18:01 +0800

    update

--- tcpcliserv/tcpcli01.c
+++ tcpcliserv/tcpcli01.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 
     if (argc != 2)
     {
-        printf("usage: tcpcli <IPaddress>");
+        err_quit("usage: tcpcli <IPaddress>");
     }
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);

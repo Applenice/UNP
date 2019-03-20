ef3e8663e968f6e122fd81b4f4441147b419dfa4
Applenice <zxapplenice@gmail.com>
2019-03-20 22:18:03 +0800

    add dgecholoop1.c

---
+++ udpcliserv/dgecholoop1.c
@@ -0,0 +1,25 @@
+#include "unp.h"
+
+static void recvfrom_int(int);
+static int count;
+
+void dg_echo(int sockfd, (struct sockaddr *) pcliaddr, socklen_t clilen)
+{
+    socklen_t len;
+    char mesg[MAXLINE];
+
+    signal(SIGINT, recvfrom_int);
+
+    for ( ; ; )
+    {
+        len = clilen;
+        recvfrom(sockfd, mesg, MAXLINE, 0, pcliaddr, &len);
+        count++;
+    }
+}
+
+static void recvfrom_int(int signo)
+{
+    printf("\nreceived %d datagrams\n", count);
+    exit(0);
+}

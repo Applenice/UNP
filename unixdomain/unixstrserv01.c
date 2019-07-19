0eafbb836d78a76cc5724919a61e3b0e416119c5
Applenice <zxapplenice@gmail.com>
2019-07-19 21:43:49 +0800

    add unixdomain/unixstrserv01.c

---
+++ unixdomain/unixstrserv01.c
@@ -0,0 +1,43 @@
+#include "unp.h"
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    pid_t childpid;
+    socklen_t clilen;
+    struct sockaddr_un cliaddr, servaddr;
+    void sig_chld(int);
+
+    listenfd = Socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    unlink(UNIXSTR_PATH);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sun_family = AF_LOCAL;
+    strcpy(servaddr.sun_path, UNIXSTR_PATH);
+
+    Bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    Listen(listenfd, LISTENQ);
+
+    Signal(SIGCHLD, sig_chld);
+
+    for ( ; ; )
+    {
+        clilen = sizeof(cliaddr);
+        if ((connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &clilen)) < 0)
+        {
+            if (errno == EINTR)
+                continue;
+            else
+                err_sys("accept error");
+        }
+
+        if ((childpid = Fork()) == 0)
+        {
+            Close(listenfd);
+            str_echo(connfd);
+            exit(0);
+        }
+        Close(connfd);
+    }
+}
